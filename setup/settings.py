79215a8a0019ac1fa21157cd3fdec39888d24891
Gabriel Lima Scheffler <44821976+scheffler5@users.noreply.github.com>
2025-03-04 12:05:45 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
 
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
