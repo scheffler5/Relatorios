fa18cc4a7f45b0900219d3fae7b46393804d42ef
Gabriel Lima Scheffler <44821976+scheffler5@users.noreply.github.com>
2025-03-05 09:20:01 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -121,11 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    
-    os.path.join(BASE_DIR, 'setup/static')
 
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
